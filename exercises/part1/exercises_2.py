42735536e81537214f10bcd6ffe98d02407a1d59
Octavian <octavian.grigorescu@codaintelligence.com>
2020-07-07 16:35:46 +0300

    Basic exercises

---
+++ exercises/part1/exercises_2.py
@@ -0,0 +1,240 @@
+#!/usr/bin/env python
+
+# ----------------------------------------#
+# Question:
+# Define a function which can compute the sum of two numbers.
+#
+# Hints:
+# Define a function with two numbers as arguments. You can compute the sum in the function and return the value.
+#
+# #----------------------------------------#
+# Question:
+# Define a function that can convert a integer into a string and print it in console.
+#
+# Hints:
+#
+# Use str() to convert a number to string.
+#
+#
+# #----------------------------------------#
+# Question:
+# Define a function that can convert a integer into a string and print it in console.
+#
+# Hints:
+#
+# Use str() to convert a number to string.
+#
+#
+# #----------------------------------------#
+# 2.10
+#
+# Question:
+# Define a function that can receive two integral numbers in string form and compute their sum and then print it in
+# console.
+#
+# Hints:
+#
+# Use int() to convert a string to integer.
+#
+#
+# #----------------------------------------#
+# 2.10
+#
+#
+# Question:
+# Define a function that can accept two strings as input and concatenate them and then print it in console.
+#
+# Hints:
+#
+# Use + to concatenate the strings
+#
+# #----------------------------------------#
+# 2.10
+#
+#
+# Question:
+# Define a function that can accept two strings as input and print the string with maximum length in console. If two
+# strings have the same length, then the function should print al l strings line by line.
+#
+# Hints:
+#
+# Use len() function to get the length of a string
+#
+#
+# #----------------------------------------#
+# 2.10
+#
+# Question:
+# Define a function that can accept an integer number as input and print the "It is an even number" if the number is
+# even, otherwise print "It is an odd number".
+#
+# Hints:
+#
+# Use % operator to check if a number is even or odd.
+#
+#
+# #----------------------------------------#
+# 2.10
+#
+# Question:
+# Define a function which can print a dictionary where the keys are numbers between 1 and 3 (both included) and the
+# values are square of keys.
+#
+# Hints:
+#
+# Use dict[key]=value pattern to put entry into a dictionary.
+# Use ** operator to get power of a number.
+#
+#
+# #----------------------------------------#
+# 2.10
+#
+# Question:
+# Define a function which can print a dictionary where the keys are numbers between 1 and 20 (both included) and the
+# values are square of keys.
+#
+# Hints:
+#
+# Use dict[key]=value pattern to put entry into a dictionary.
+# Use ** operator to get power of a number.
+# Use range() for loops.
+#
+#
+# #----------------------------------------#
+# 2.10
+#
+# Question:
+# Define a function which can generate a dictionary where the keys are numbers between 1 and 20 (both included) and
+# the values are square of keys. The function should just print the values only.
+#
+# Hints:
+#
+# Use dict[key]=value pattern to put entry into a dictionary.
+# Use ** operator to get power of a number.
+# Use range() for loops.
+# Use keys() to iterate keys in the dictionary. Also we can use item() to get key/value pairs.
+#
+# #----------------------------------------#
+# 2.10
+#
+# Question:
+# Define a function which can generate a dictionary where the keys are numbers between 1 and 20 (both included) and the
+# values are square of keys. The function should just print the keys only.
+#
+# Hints:
+#
+# Use dict[key]=value pattern to put entry into a dictionary.
+# Use ** operator to get power of a number.
+# Use range() for loops.
+# Use keys() to iterate keys in the dictionary. Also we can use item() to get key/value pairs.
+#
+#
+# #----------------------------------------#
+# 2.10
+#
+# Question:
+# Define a function which can generate and print a list where the values are square of numbers between
+# 1 and 20 (both included).
+#
+# Hints:
+#
+# Use ** operator to get power of a number.
+# Use range() for loops.
+# Use list.append() to add values into a list.
+#
+#
+# #----------------------------------------#
+# 2.10
+#
+# Question:
+# Define a function which can generate a list where the values are square of numbers between 1 and 20 (both included).
+# Then the function needs to print the first 5 elements in the list.
+#
+# Hints:
+#
+# Use ** operator to get power of a number.
+# Use range() for loops.
+# Use list.append() to add values into a list.
+# Use [n1:n2] to slice a list
+#
+#
+# #----------------------------------------#
+# 2.10
+#
+# Question:
+# Define a function which can generate a list where the values are square of numbers between 1 and 20 (both included).
+# Then the function needs to print the last 5 elements in the list.
+#
+# Hints:
+#
+# Use ** operator to get power of a number.
+# Use range() for loops.
+# Use list.append() to add values into a list.
+# Use [n1:n2] to slice a list
+#
+#
+# #----------------------------------------#
+# 2.10
+#
+# Question:
+# Define a function which can generate a list where the values are square of numbers between 1 and 20 (both included).
+# Then the function needs to print all values except the first 5 elements in the list.
+#
+# Hints:
+#
+# Use ** operator to get power of a number.
+# Use range() for loops.
+# Use list.append() to add values into a list.
+# Use [n1:n2] to slice a list
+#
+#
+# #----------------------------------------#
+# 2.10
+#
+# Question:
+# Define a function which can generate and print a tuple where the value are square of numbers between
+# 1 and 20 (both included).
+#
+# Hints:
+#
+# Use ** operator to get power of a number.
+# Use range() for loops.
+# Use list.append() to add values into a list.
+# Use tuple() to get a tuple from a list.
+#
+#
+# #----------------------------------------#
+# 2.10
+#
+# Question:
+# With a given tuple (1,2,3,4,5,6,7,8,9,10), write a program to print the first half values in one line and the last
+# half values in one line.
+#
+# Hints:
+#
+# Use [n1:n2] notation to get a slice from a tuple.
+#
+#
+# #----------------------------------------#
+# 2.10
+#
+# Question:
+# Write a program to generate and print another tuple whose values are even numbers in the given
+# tuple (1,2,3,4,5,6,7,8,9,10).
+#
+# Hints:
+#
+# Use "for" to iterate the tuple
+# Use tuple() to generate a tuple from a list.
+#
+#
+# #----------------------------------------#
+# 2.14
+#
+# Question:
+# Write a program which accepts a string as input to print "Yes" if the string is "yes" or "YES" or "Yes",
+# otherwise print "No".
+#
+# Hints:
+#
+# Use if statement to judge condition.
---
+++ exercises/part1/exercises_3.py
@@ -0,0 +1,317 @@
+# # ----------------------------------------#
+# 3.4
+#
+# Question:
+# Write a program which can filter even numbers in a list by using filter function. The list is: [1,2,3,4,5,6,7,8,9,10].
+#
+# Hints:
+#
+# Use filter() to filter some elements in a list.
+# Use lambda to define anonymous functions.
+#
+#
+# #----------------------------------------#
+# 3.4
+#
+# Question:
+# Write a program which can map() to make a list whose elements are square of elements in [1,2,3,4,5,6,7,8,9,10].
+#
+# Hints:
+#
+# Use map() to generate a list.
+# Use lambda to define anonymous functions.
+#
+# #----------------------------------------#
+# 3.5
+#
+# Question:
+# Write a program which can map() and filter() to make a list whose elements are square of even number in
+# [1,2,3,4,5,6,7,8,9,10].
+#
+# Hints:
+#
+# Use map() to generate a list.
+# Use filter() to filter elements of a list.
+# Use lambda to define anonymous functions.
+#
+#
+# #----------------------------------------#
+# 3.5
+#
+# Question:
+# Write a program which can filter() to make a list whose elements are even number between 1 and 20 (both included).
+#
+# Hints:
+#
+# Use filter() to filter elements of a list.
+# Use lambda to define anonymous functions.
+#
+#
+# #----------------------------------------#
+# 3.5
+#
+# Question:
+# Write a program which can map() to make a list whose elements are square of numbers between 1 and 20 (both included).
+#
+# Hints:
+#
+# Use map() to generate a list.
+# Use lambda to define anonymous functions.
+#
+#
+# #----------------------------------------#
+# 7.2
+#
+# Question:
+# Define a class named American which has a static method called printNationality.
+#
+# Hints:
+#
+# Use @staticmethod decorator to define class static method.
+#
+#
+# #----------------------------------------#
+#
+# 7.2
+#
+# Question:
+# Define a class named American and its subclass NewYorker.
+#
+# Hints:
+#
+# Use class Subclass(ParentClass) to define a subclass.
+#
+#
+# #----------------------------------------#
+#
+# 7.2
+#
+# Question:
+# Define a class named Circle which can be constructed by a radius. The Circle class has a method which can compute
+# the area.
+#
+# Hints:
+#
+# Use def methodName(self) to define a method.
+#
+#
+# 7.2
+#
+# Define a class named Rectangle which can be constructed by a length and width. The Rectangle class has a method
+# which can compute the area.
+#
+# Hints:
+#
+# Use def methodName(self) to define a method.
+#
+#
+# 7.2
+#
+# Define a class named Shape and its subclass Square. The Square class has an init function which takes a length as
+# argument. Both classes have a area function which can print the area of the shape where Shape's area is 0 by default.
+#
+# Hints:
+#
+# To override a method in super class, we can define a method with the same name in the super class.
+#
+#
+#
+# Please raise a RuntimeError exception.
+#
+# Hints:
+#
+# Use raise() to raise an exception.
+#
+# Write a function to compute 5/0 and use try/except to catch the exceptions.
+#
+# Hints:
+#
+# Use try/except to catch exceptions.
+#
+# Define a custom exception class which takes a string message as attribute.
+#
+# Hints:
+#
+# To define a custom exception, we need to define a class inherited from Exception.
+#
+# Question:
+#
+# Assuming that we have some email addresses in the "username@companyname.com" format, please write program to print
+# the user name of a given email address. Both user names and company names are composed of letters only.
+#
+# Example:
+# If the following email address is given as input to the program:
+#
+# john@google.com
+#
+# Then, the output of the program should be:
+#
+# john
+#
+# In case of input data being supplied to the question, it should be assumed to be a console input.
+#
+# Hints:
+#
+# Use \w to match letters.
+#
+# Question:
+#
+# Assuming that we have some email addresses in the "username@companyname.com" format, please write program to print
+# the company name of a given email address. Both user names and company names are composed of letters only.
+#
+# Example:
+# If the following email address is given as input to the program:
+#
+# john@google.com
+#
+# Then, the output of the program should be:
+#
+# google
+#
+# In case of input data being supplied to the question, it should be assumed to be a console input.
+#
+# Hints:
+#
+# Use \w to match letters.
+#
+# Question:
+#
+# Write a program which accepts a sequence of words separated by whitespace as input to print the words composed of
+# digits only.
+#
+# Example:
+# If the following words is given as input to the program:
+#
+# 2 cats and 3 dogs.
+#
+# Then, the output of the program should be:
+#
+# ['2', '3']
+#
+# In case of input data being supplied to the question, it should be assumed to be a console input.
+#
+# Hints:
+#
+# Use re.findall() to find all substring using regex.
+#
+# Question:
+#
+#
+# Print a unicode string "hello world".
+#
+# Hints:
+#
+# Use u'strings' format to define unicode string.
+#
+# Write a program to read an ASCII string and to convert it to a unicode string encoded by utf-8.
+#
+# Hints:
+#
+# Use unicode() function to convert.
+#
+# Question:
+#
+# Write a special comment to indicate a Python source code file is in unicode.
+#
+# Hints:
+#
+# Question:
+#
+# Write a program to compute 1/2+2/3+3/4+...+n/n+1 with a given n input by console (n>0).
+#
+# Example:
+# If the following n is given as input to the program:
+#
+# 5
+#
+# Then, the output of the program should be:
+#
+# 3.55
+#
+# In case of input data being supplied to the question, it should be assumed to be a console input.
+#
+# Hints:
+# Use float() to convert an integer to a float
+#
+# Question:
+#
+# Write a program to compute:
+#
+# f(n)=f(n-1)+100 when n>0
+# and f(0)=1
+#
+# with a given n input by console (n>0).
+#
+# Example:
+# If the following n is given as input to the program:
+#
+# 5
+#
+# Then, the output of the program should be:
+#
+# 500
+#
+# In case of input data being supplied to the question, it should be assumed to be a console input.
+#
+# Hints:
+# We can define recursive function in Python.
+#
+#
+# Question:
+#
+#
+# The Fibonacci Sequence is computed based on the following formula:
+#
+#
+# f(n)=0 if n=0
+# f(n)=1 if n=1
+# f(n)=f(n-1)+f(n-2) if n>1
+#
+# Please write a program to compute the value of f(n) with a given n input by console.
+#
+# Example:
+# If the following n is given as input to the program:
+#
+# 7
+#
+# Then, the output of the program should be:
+#
+# 13
+#
+# In case of input data being supplied to the question, it should be assumed to be a console input.
+#
+# Hints:
+# We can define recursive function in Python.
+#
+#
+#
+# #----------------------------------------#
+#
+# Question:
+#
+# The Fibonacci Sequence is computed based on the following formula:
+#
+#
+# f(n)=0 if n=0
+# f(n)=1 if n=1
+# f(n)=f(n-1)+f(n-2) if n>1
+#
+# Please write a program using list comprehension to print the Fibonacci Sequence in comma separated form with a
+# given n input by console.
+#
+# Example:
+# If the following n is given as input to the program:
+#
+# 7
+#
+# Then, the output of the program should be:
+#
+# 0,1,1,2,3,5,8,13
+#
+#
+# Hints:
+# We can define recursive function in Python.
+# Use list comprehension to generate a list from an existing list.
+# Use string.join() to join a list of strings.
+#
+# In case of input data being supplied to the question, it should be assumed to be a console input.
