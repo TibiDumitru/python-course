77ea4a2040aebe8367ade9718d5b501e49188d0c
Andrei-Tiberiu Dumitru <dtiberiu100@gmail.com>
2020-07-07 23:50:46 +0300

    challenges V

--- challenges/hangman.py
+++ challenges/hangman.py
@@ -50,3 +50,145 @@
 #    |      / \
 #    |
 # -------
+import random
+import urllib.request
+import json
+
+
+stage_a = """
+        You are safe now!
+            --------
+           |       |
+           |       
+           |     
+           |      
+           |      
+           |
+        -------
+        """
+
+stage_b = """
+        Not great, not terrible!
+            --------
+           |       |
+           |       O
+           |      
+           | 
+           | 
+           |
+        -------
+        """
+
+stage_c = """
+        You can still do this!
+            --------
+           |       |
+           |       O
+           |      / \\
+           | 
+           | 
+           |
+        -------
+        """
+
+stage_d = """
+        You are in danger!
+            --------
+           |       |
+           |       O
+           |      /|\\
+           |       |
+           | 
+           |
+        -------
+        """
+
+stage_e = """
+        You are dead!
+            --------
+           |       |
+           |       O
+           |      /|\\
+           |       |
+           |      / \\
+           |
+        -------
+        """
+
+state = {0: stage_a, 1: stage_b, 2: stage_c, 3: stage_d, 4: stage_e}
+
+
+def get_word():
+    with urllib.request.urlopen("https://www.randomlists.com/data/words.json") as url:
+        data = json.loads(url.read().decode())
+        while True:
+            chosen_word = random.choice(data['data']).upper()
+            if len(chosen_word) > 4:
+                return chosen_word
+
+
+def play(username, chosen_word):
+    mistakes = 0
+    finished = False
+    no_letters = len(chosen_word)
+    first = chosen_word[0]
+    last = chosen_word[no_letters - 1]
+    current_word = no_letters * ['_']
+    missing_letters = set()
+
+    for i in range(no_letters):
+        if chosen_word[i] == first:
+            current_word[i] = first
+        elif chosen_word[i] == last:
+            current_word[i] = last
+        else:
+            missing_letters.add(chosen_word[i])
+
+    while not finished:
+        print(state[mistakes])
+        if mistakes == 4:
+            print('Sorry, ' + username + '! GAME OVER!')
+            finished = True
+            continue
+        print("".join(current_word))
+        print()
+        print('Choose a letter:')
+        chosen_letter = input()
+        if not chosen_letter.isalpha() or len(chosen_letter) > 1:
+            print('Wrong input, try again!')
+            continue
+        if chosen_letter.islower():
+            chosen_letter = chosen_letter.upper()
+        if chosen_letter in missing_letters:
+            print('Good job, ' + username + '!')
+            for i in range(no_letters):
+                if chosen_word[i] == chosen_letter:
+                    current_word[i] = chosen_letter
+            missing_letters.remove(chosen_letter)
+            if len(missing_letters) == 0:
+                print('\nCongratulations, ' + username + '! You WIN!')
+                print('The word was: ' + "".join(current_word))
+                finished = True
+        else:
+            print('Nope, try something else!')
+            mistakes += 1
+
+
+def main():
+    print('============== Welcome to Hangman! ================')
+    print('Please choose a nickname: ')
+    username = input()
+    print("Let's play, " + username + "!")
+    while True:
+        chosen_word = get_word()
+        play(username, chosen_word)
+        print()
+        print('===============================================')
+        print('Wanna play again, ' + username + '? (Y / N)')
+        ans = input().upper()
+        if ans == 'N':
+            break
+
+
+if __name__ == "__main__":
+    main()
--- challenges/tic_tac_toe.py
+++ challenges/tic_tac_toe.py
@@ -23,3 +23,79 @@
 #  |   |   | O |
 #
 #   --- --- ---
+
+
+def display(table):
+    print('--- --- ---')
+    for i in range(3):
+        line = ''
+        for j in range(3):
+            line += '|'
+            line += table[i][j]
+            line += '| '
+        print(line)
+        print('--- --- ---')
+
+
+def check_win(table):
+    if table[0][0] == table[1][1] and table[1][1] == table[2][2]:
+        if table[0][0] != ' ':
+            return True
+    for i in range(3):
+        if table[i][0] == table[i][1] and table[i][1] == table[i][2]:
+            if table[i][0] != ' ':
+                return True
+        if table[0][i] == table[1][i] and table[1][i] == table[2][i]:
+            if table[0][i] != ' ':
+                return True
+    return False
+
+
+def play(table, player, symbol):
+    print(player + "'s turn!")
+    ok = False
+    while not ok:
+        input1 = input()
+        pos = input1.split()
+        if not pos[0].isdigit() or not pos[1].isdigit():
+            print('Invalid input! Try again, ' + player + '!')
+            continue
+        x = int(pos[0])
+        y = int(pos[1])
+        if x < 0 or x > 2 or y < 0 or y > 2:
+            print('Invalid position! Try again, ' + player + '!')
+            continue
+        if table[x][y] == 'X' or table[x][y] == '0':
+            print('Position taken! Try again, ' + player + '!')
+            continue
+        table[x][y] = symbol[player]
+        ok = True
+
+    display(table)
+    if check_win(table):
+        print(player + ' wins!')
+        return True
+    return False
+
+
+def main():
+    print('============== Welcome to TIC-TAC-TOE! ================')
+    print('Player1 (X), choose a nickname: ')
+    player1 = input()
+    print('Player2 (0), choose a nickname: ')
+    player2 = input()
+    symbol = {player1: 'X', player2: '0'}
+    table = [[' ' for _j in range(3)] for _i in range(3)]
+    finished = False
+    while not finished:
+        for i in range(2):
+            if i == 0:
+                finished = play(table, player1, symbol)
+            else:
+                finished = play(table, player2, symbol)
+            if finished:
+                break
+
+
+if __name__ == "__main__":
+    main()
